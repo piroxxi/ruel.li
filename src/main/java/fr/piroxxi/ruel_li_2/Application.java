634df367b7705e396526f4087d7bb4b2bf670eb0
Raphael POITTEVIN <piroxxi@gmail.com>
2024-05-13 15:07:41 -0400

    Project creation (via Intellij)

---
+++ src/main/java/fr/piroxxi/ruel_li_2/Application.java
@@ -0,0 +1,13 @@
+package fr.piroxxi.ruel_li_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/fr/piroxxi/ruel_li_2/ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.piroxxi.ruel_li_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
